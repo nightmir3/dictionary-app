27f29146edb018c174ad368f1041d816f62dde58
Matias Baldanza <matiasbaldanza@gmail.com>
2023-03-29 13:03:00 -0300

    Style: add LanguageToggle to Header (styles only) using TailwindCSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import LanguageToggle from './LanguageToggle/LanguageToggle'
 import { HiOutlineBookOpen, HiOutlineMoon, HiOutlineSun, HiChevronDown } from 'react-icons/hi'
 
 function Header () {
   return (
     <div className='flex justify-between text-gray-500'>
       <div><HiOutlineBookOpen className='text-4xl' /></div>
-      <div className='flex gap-3 divide-x-2 items-center'>
-        <span className='text-sm font-bold'>Serif <HiChevronDown className='text-purple-600 text-2xl ml-1 inline' /></span>
-        <span className='pl-3'><HiOutlineMoon className='text-2xl' /></span>
+      <div className='flex items-center gap-3'>
+        <span className='text-sm font-bold'>Serif <HiChevronDown className='inline ml-1 text-2xl text-purple-600' /></span>
+        <LanguageToggle />
+        <span className='pl-2'><HiOutlineMoon className='text-2xl' /></span>
       </div>
     </div>
   )
 }
-
 
 export default Header
