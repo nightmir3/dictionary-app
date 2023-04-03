f4fd83f96ace650162ce7639e5d2cfe8728d38c2
Matias Baldanza <matiasbaldanza@gmail.com>
2023-04-03 13:57:21 -0300

    style: add autofocus and outline styling on :focus for SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { HiOutlineSearch } from 'react-icons/hi'
 
-function SearchBar () {
+function SearchBar ({ onSubmit }) {
   const searchInput = React.useId()
+  const [input, setInput] = React.useState('')
+
   return (
     <form
       className='flex w-full gap-4 px-5 py-3 bg-gray-100 rounded-2xl'
+      action='submit'
+      onSubmit={(event) => {
+        event.preventDefault()
+        onSubmit(input)
+      }}
     >
       <label
         className='sr-only'
@@ -14,9 +21,12 @@ function SearchBar () {
       </label>
       <input
         id={searchInput}
-        className='w-full text-sm font-bold text-black md:text-base bg-inherit'
+        className='w-full text-sm font-bold text-black md:text-base bg-inherit outline-offset-8 outline-purple-600'
         type='text'
-        value='keyboard'
+        value={input}
+        autoFocus
+        placeholder='Type here...'
+        onChange={(event) => setInput(event.target.value)}
       />
       <button
         type='submit'
