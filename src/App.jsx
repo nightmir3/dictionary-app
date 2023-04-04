e8223306753dcfed5059d8de5e1d1e413ebd028b
Matias Baldanza <matiasbaldanza@gmail.com>
2023-04-04 12:31:32 -0300

    feat: add loader/spinner

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SearchBar from './components/SearchBar'
 import ResultsPane from './components/ResultsPane'
 import fetchWord from './utils/fetcher'
 import InitialContent from './components/InitialContent'
+import { Ring } from '@uiball/loaders'
 
 function App () {
   const [activeFont, setActiveFont] = useState('font-serif')
@@ -27,15 +28,23 @@ function App () {
 
   return (
     <div
-      className={`${activeFont} container p-6 md:p-12 mx-auto flex flex-col gap-7 md:gap-10 lg:max-w-4xl dark:text-slate-100 dark:bg-gray-900`}
+      className={`${activeFont} container p-6 md:p-12 mx-auto flex flex-col gap-7 md:gap-10 lg:max-w-4xl dark:text-slate-100 dark:bg-gray-900 h-screen`}
     >
       <Header />
       <SearchBar onSubmit={handleSearch} />
-      {/* TODO: add loader */}
-      {fetchStatus === 'idle' && <InitialContent message='Welcome' />}
-      {fetchStatus === 'fetching' && <InitialContent message='Loading' />}
-      {fetchStatus === 'error' && <InitialContent message='Not Found' />}
-      {fetchStatus === 'success' && <ResultsPane res={wordData} />}
+
+      <div className='flex-1 grid place-items-center'>
+        {fetchStatus === 'idle' && <InitialContent message='Welcome' />}
+        {fetchStatus === 'fetching' &&
+          <Ring
+            size={40}
+            lineWeight={5}
+            speed={2}
+            color='#8e24aa' // text-purple-600
+          />}
+        {fetchStatus === 'error' && <InitialContent message='Not Found' />}
+        {fetchStatus === 'success' && <ResultsPane res={wordData} />}
+      </div>
     </div>
   )
 }
