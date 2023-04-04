53e4a09e95e6fe109501432a0d6e67d9fbb35eb1
Matias Baldanza <matiasbaldanza@gmail.com>
2023-04-04 13:38:36 -0300

    feat: add tooltips to AudioPlayer ('No audio available' if disabled, or 'Source: URL')

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -11,7 +11,8 @@ function AudioPlayer ({ audioUrl }) {
         className={`${!audioUrl
                         ? 'bg-gray-200 text-gray-600'
                         : 'text-purple-600 bg-purple-200'} 
-                    p-4 aspect-square rounded-[50%] `}
+                    p-4 aspect-square rounded-[50%]`}
+        title={`${!audioUrl ? 'No audio available' : `Source: ${audioUrl}`}`}
         onClick={() => audioRef.current.play()}
       >
         <HiPlay className='text-md' />
