653ccd874f940ad06e26f00e40f9d191c5af2012
Matias Baldanza <matiasbaldanza@gmail.com>
2023-04-05 11:53:17 -0300

    feat: play button animates on :active, only when clickable

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -11,8 +11,8 @@ function AudioPlayer ({ audioUrl }) {
       <button
         disabled={!audioUrl}
         className={`${!audioUrl
-                        ? 'bg-gray-200 text-gray-600'
-                        : 'text-purple-600 bg-purple-200'} 
+                        ? 'bg-gray-200 text-gray-400 cursor-not-allowed'
+                        : 'text-purple-600 bg-purple-200 active:translate-y-0.5'} 
                      relative flex p-4 aspect-square rounded-[50%]`}
         title={`${!audioUrl ? 'No audio available' : `Source: ${audioUrl}`}`}
         onClick={() => {
@@ -20,7 +20,7 @@ function AudioPlayer ({ audioUrl }) {
           audioRef.current.play()
         }}
       >
-        <HiPlay className='text-md' />
+        <HiPlay className='text-md transition-none' />
         {/* animate outline when playing */
           audioIsPlaying &&
             <div className='h-full w-full border-purple-600 border-4 aspect-square rounded-[50%] inset-0 animate-ping absolute' />
