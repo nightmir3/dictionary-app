5daea188cdf1b082c9d6378a2edf283bc3b2e9c4
Matias Baldanza <matiasbaldanza@gmail.com>
2023-04-05 11:08:57 -0300

    feat: play button animates on play, stops animation onEnded

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -3,6 +3,8 @@ import { HiPlay } from 'react-icons/hi2'
 
 function AudioPlayer ({ audioUrl }) {
   const audioRef = React.useRef()
+  const [audioIsPlaying, setAudioIsPlaying] = React.useState(false)
+
   console.log(audioUrl)
   return (
     <div>
@@ -11,13 +13,24 @@ function AudioPlayer ({ audioUrl }) {
         className={`${!audioUrl
                         ? 'bg-gray-200 text-gray-600'
                         : 'text-purple-600 bg-purple-200'} 
-                    p-4 aspect-square rounded-[50%]`}
+                     relative flex p-4 aspect-square rounded-[50%]`}
         title={`${!audioUrl ? 'No audio available' : `Source: ${audioUrl}`}`}
-        onClick={() => audioRef.current.play()}
+        onClick={() => {
+          setAudioIsPlaying(true)
+          audioRef.current.play()
+        }}
       >
         <HiPlay className='text-md' />
+        {/* animate outline when playing */
+          audioIsPlaying &&
+            <div className='h-full w-full border-purple-600 border-4 aspect-square rounded-[50%] inset-0 animate-ping absolute' />
+        }
       </button>
-      <audio ref={audioRef} src={audioUrl} />
+      <audio
+        ref={audioRef}
+        src={audioUrl}
+        onEnded={() => setAudioIsPlaying(false)}
+      />
     </div>
   )
 }
